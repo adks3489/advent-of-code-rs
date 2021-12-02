b3808fbecf15e2903ff8095c724e1a35b5c8e1c9
James Chiang <adks3489@gmail.com>
2021-12-02 16:23:08 +0800

    feat: day2 part1

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,46 @@ fn day1() {
     println!("{}", count);
 }
 
+enum Direction {
+    Forward,
+    Up,
+    Down,
+}
+struct Command {
+    direction: Direction,
+    distance: i32,
+}
+impl Command {
+    fn new(input: &str) -> Self {
+        let col: Vec<&str> = input.split(" ").collect();
+        Command {
+            direction: match col[0] {
+                "forward" => Direction::Forward,
+                "up" => Direction::Up,
+                "down" => Direction::Down,
+                _ => panic!("unknown direction: {}", col[0]),
+            },
+            distance: col[1].parse::<i32>().unwrap(),
+        }
+    }
+}
+fn day2() {
+    let contnets = fs::read_to_string("input/day2.txt").unwrap();
+    let lines: Vec<&str> = contnets.lines().collect();
+
+    let mut horizontal_position = 0;
+    let mut depth = 0;
+    lines
+        .iter()
+        .map(|l| Command::new(l))
+        .for_each(|c| match c.direction {
+            Direction::Forward => horizontal_position += c.distance,
+            Direction::Up => depth -= c.distance,
+            Direction::Down => depth += c.distance,
+        });
+    println!("{}", horizontal_position * depth);
+}
+
 fn main() {
-    day1();
+    day2();
 }
