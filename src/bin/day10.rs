26f865f00aa023394bee5596ab065773399f37ab
James Chiang <adks3489@gmail.com>
2021-12-10 16:24:57 +0800

    refactor: merge match pattern

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -6,10 +6,7 @@ fn calc(data: &Vec<&str>) -> i32 {
             let mut stack = Vec::new();
             for c in line.chars() {
                 match c {
-                    '(' => stack.push(c),
-                    '[' => stack.push(c),
-                    '{' => stack.push(c),
-                    '<' => stack.push(c),
+                    '(' | '[' | '{' | '<' => stack.push(c),
                     ')' => {
                         if stack.pop() != Some('(') {
                             return 3;
@@ -45,10 +42,7 @@ fn calc2(data: &Vec<&str>) -> usize {
             let mut stack = Vec::new();
             for c in line.chars() {
                 match c {
-                    '(' => stack.push(c),
-                    '[' => stack.push(c),
-                    '{' => stack.push(c),
-                    '<' => stack.push(c),
+                    '(' | '[' | '{' | '<' => stack.push(c),
                     ')' => {
                         if stack.pop() != Some('(') {
                             return None;
